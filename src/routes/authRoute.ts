aa183f7582b5cc68496d8c332a648b2f3d9a185d
Youssef-k-Shebl <youssef.kamel.shebl88@gmail.com>
2025-08-06 15:43:06 +0300

    get validation list from requests

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,15 +1,16 @@
 import express from "express";
-import { loginValidator, signupValidator } from "../validators/userValidator";
 import { validate } from "../middlewares/validate";
 import { authMiddleware } from "../middlewares/auth";
 import { AuthController } from "../controllers/authController";
+import { LoginRequest } from "../dto/requests/LoginRequest";
+import { SignupRequest } from "../dto/requests/SignupRequest";
 
 export const router = express.Router();
 
 const controller = new AuthController();
-router.post("/login", loginValidator, validate, controller.login);
+router.post("/login", LoginRequest.getValidationList(), validate, controller.login);
 
-router.post("/signup", signupValidator, validate, controller.signup);
+router.post("/signup", SignupRequest.getValidationList(), validate, controller.signup);
 
 router.get("/profile", authMiddleware, controller.profile);
 
